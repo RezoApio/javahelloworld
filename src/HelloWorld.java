d1233fe8708be316e0ce82b265225032ee2ee5f7
RezoApio <william.dupre@gmail.com>
2017-05-31 15:28:07 +0000

    Second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
     public static void main (String [] args)
     {
-         System.out.println("Hello World My Toto");
+         System.out.println("Hello World My Toto with Prod CI");
     }
 }
 
